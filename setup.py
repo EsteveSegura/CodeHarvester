fb69c598f25b276d30271a8f9f453d124cb17550
Esteve Segura <esteve.segura@voicemod.net>
2024-03-15 00:21:08 +0100

    feat: update setup.py with project details and dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
 setup(
-    ext_modules = cythonize("./src/main.py")
+    name='CodeHarvester',
+    version='1.0.0',
+    author='Esteve Segura',
+    author_email='estevesegura.gir@gmail.com',
+    description='CodeHarvester efficiently aggregates code and text from files for streamlined AI analysis, simplifying data compilation and preparation.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/EsteveSegura/CodeHarvester',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'Cython',
+        'PyInstaller',
+        'Flask',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'codeharvester=main:main',
+        ],
+    },
 )
